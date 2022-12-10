9f8dabf48db45458a54ca7ab98053f8d47f7f3e0
Karen <karenjawane28@gmail.com>
2022-12-10 16:31:10 -0300

    first commit

---
+++ dom.js
@@ -0,0 +1,17 @@
+function alternancia(){
+    var element="document.body";
+    element.classList.body.toggle("alternancia")
+
+}
+
+function enviar(){
+    let nome = document.querySelectorAll("nome");
+    let email= document.querySelectorAll("email")
+}
+if(nome !== "" && email !==){
+    alert("True")
+}
+else{
+    alert("False")
+}
+
